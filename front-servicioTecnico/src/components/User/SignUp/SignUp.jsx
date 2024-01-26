e6954f66c96dc33f8d101b57a642dfd6f371e8bf
Daniel Ceriana <danielcerianax@gmail.com>
2024-01-26 11:59:38 -0300

    quitadas lineas innecesarias

--- front-servicioTecnico/src/components/User/SignUp/SignUp.jsx
+++ front-servicioTecnico/src/components/User/SignUp/SignUp.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import axios from "axios"
-import { toast } from "react-toastify";
 import { useDispatch } from "react-redux";
 import { signUp } from "../../../redux/actions/userActions";
 
